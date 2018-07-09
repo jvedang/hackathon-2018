67b171db80e4614e8091f05e22c853727ac71b05
Vedang Jadhav <jvedang@Vedangs-MacBook-Pro.local>
2018-07-08 20:51:07 -0700

    Adding post method

--- routes/hackathon.js
+++ routes/hackathon.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 router.post('/hackathonServices', function(req, res) {
 
-    var actionType = req.body;
+    var actionType = req.body.queryResult.parameters['actionType'];
 
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({"fulfillmentText" : actionType}));
