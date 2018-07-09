e71d88e3dbde17dd514b68d9c337286a6269f406
Vedang Jadhav <jvedang@Vedangs-MacBook-Pro.local>
2018-07-08 19:33:28 -0700

    Adding post method

--- routes/index.js
+++ routes/index.js
@@ -12,9 +12,9 @@ router.get('/getPromos', function(req, res, next) {
 
 router.post('/getPromos', function(req, res, next) {
 
-    var value = req.body.result.parameters['Promos'];
+    //var value = req.body.result.parameters['Promos'];
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({"fulfillmentText" : "You have 20% off now on "+ value +" !"}));
+    res.send(JSON.stringify({"fulfillmentText" : "You have 20% off now on !"}));
 });
 
 module.exports = router;
