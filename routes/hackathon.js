fa47140ef4f82f4478fa51587f94be3d6e086684
Vedang Jadhav <jvedang@gmail.com>
2018-07-11 14:55:50 -0700

    Update hackathon.js

--- routes/hackathon.js
+++ routes/hackathon.js
@@ -4,6 +4,7 @@ var router = express.Router();
 
 router.post('/hackathonServices', function(req, res) {
 
+    console.log(req.body);
     var actionType = req.body.queryResult.parameters['actionType'];
     var firstName = req.body.queryResult.parameters['firstName'];
     var lastName = req.body.queryResult.parameters['lastName'];
